8feb1e372c6d8ca9bf6e30d253ad29d903c3f4f3
robert-alk <86311278+robert-alk@users.noreply.github.com>
2024-01-18 11:10:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '.')));
 
-app.listen(3000, function () {
- console.log('App is listening on port 3000!');
+app.listen(80, function () {
+ console.log('App is listening on port 80!');
 });
